99f8fd581842e6c23e563e595c3a6819f88482fd
Curtis Rueden <ctrueden@wisc.edu>
2017-06-20 21:48:33 +0200

    update-to-latest.sh: tell the user what happened

--- update-to-latest.sh
+++ update-to-latest.sh
@@ -13,4 +13,18 @@
 # You can also cd into any of the repositories here and
 # operate on them directly like normal git repositories.
 
-git submodule update --recursive --remote
+git submodule update --recursive --remote &&
+
+echo "
+Updated all repositories to the latest commit on master.
+
+You might find that "git status" reports something like:
+
+   modified:   imagej-tutorials (new commits)
+
+But that is OK! It means you have a newer version that what
+was registered with this repository at the time. No worries.
+
+You can also cd into any of the repositories here and
+operate on them directly like normal git repositories.
+"
