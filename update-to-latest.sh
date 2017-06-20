65670b3cc13e980e9341688e3cdd78cd94099b10
Curtis Rueden <ctrueden@wisc.edu>
2017-06-20 21:51:15 +0200

    update-to-latest.sh: remove old comment; fix typo

--- update-to-latest.sh
+++ update-to-latest.sh
@@ -2,16 +2,6 @@
 
 # Run this script to update all submodule repositories here
 # to their latest version on their respective master branches.
-#
-# Afterward, "git status" may report something like:
-#
-#    modified:   imagej-tutorials (new commits)
-#
-# But that is OK! It means you have a newer version that what
-# was registered with this repository at the time. No worries.
-#
-# You can also cd into any of the repositories here and
-# operate on them directly like normal git repositories.
 
 git submodule update --recursive --remote &&
 
@@ -22,7 +12,7 @@ You might find that "git status" reports something like:
 
    modified:   imagej-tutorials (new commits)
 
-But that is OK! It means you have a newer version that what
+But that is OK! It means you have a newer version than what
 was registered with this repository at the time. No worries.
 
 You can also cd into any of the repositories here and
